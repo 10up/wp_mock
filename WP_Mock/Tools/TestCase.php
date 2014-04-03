5b250d684595284a2fe499571b74cad6a3253f03
John P. Bloch <john.bloch@10up.com>
2014-04-03 12:13:31 -0400

    Add method for mocking static methods

--- WP_Mock/Tools/TestCase.php
+++ WP_Mock/Tools/TestCase.php
@@ -3,7 +3,9 @@
 namespace WP_Mock\Tools;
 
 use WP_Mock;
+use Mockery;
 use Exception;
+use ReflectionMethod;
 use WP_Mock\Tools\Constraints\ExpectationsMet;
 use WP_Mock\Tools\Constraints\IsEqualHTML;
 
@@ -114,6 +116,61 @@ class TestCase extends \PHPUnit_Framework_TestCase {
 	public function assertEqualsHTML( $expected, $actual, $message = '' ) {
 		$constraint = new IsEqualHTML( $expected );
 		$this->assertThat( $actual, $constraint, $message );
+	}
+
+	/**
+	 * Mock a static method of a class
+	 *
+	 * @param string      $class  The classname or class::method name
+	 * @param null|string $method The method name. Optional if class::method used for $class
+	 *
+	 * @return \Mockery\Expectation
+	 * @throws Exception
+	 */
+	protected function mockStaticMethod( $class, $method = null ) {
+		if ( ! $method ) {
+			list( $class, $method ) = ( explode( '::', $class ) + array( null, null ) );
+		}
+		if ( ! $method ) {
+			throw new Exception( sprintf( 'Could not mock %s::%s', $class, $method ) );
+		}
+		if ( ! WP_Mock::usingPatchwork() || ! function_exists( 'Patchwork\Interceptor\patch' ) ) {
+			throw new Exception( 'Patchwork is not loaded! Please load patchwork before mocking static methods!' );
+		}
+
+		$safe_method = "wp_mock_safe_$method";
+		$signature   = md5( "$class::$method" );
+		if ( ! empty( $this->mockedStaticMethods[$signature] ) ) {
+			$mock = $this->mockedStaticMethods[$signature];
+		} else {
+
+			$rMethod = false;
+			if ( class_exists( $class ) ) {
+				$rMethod = new ReflectionMethod( $class, $method );
+			}
+			if (
+				$rMethod &&
+				(
+					! $rMethod->isUserDefined() ||
+					! $rMethod->isStatic() ||
+					$rMethod->isPrivate()
+				)
+			) {
+				throw new Exception( sprintf( '%s::%s is not a user-defined non-private static method!', $class, $method ) );
+			}
+
+			/** @var \Mockery\Mock $mock */
+			$mock = Mockery::mock( $class );
+			$mock->shouldAllowMockingProtectedMethods();
+			$this->mockedStaticMethods[$signature] = $mock;
+
+			\Patchwork\Interceptor\patch( "$class::$method", function () use ( $mock, $safe_method ) {
+				return call_user_func_array( array( $mock, $safe_method ), func_get_args() );
+			}, ! ( $rMethod ) );
+		}
+		$expectation = $mock->shouldReceive( $safe_method );
+
+		return $expectation;
 	}
 
 	protected function cleanGlobals() {
