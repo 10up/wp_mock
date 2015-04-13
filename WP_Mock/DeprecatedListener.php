3e4d170e874be34b6434d0785630f49d3ed1e83a
John P Bloch <john.bloch@10up.com>
2015-04-13 00:30:41 -0500

    More deprecated method functionality
    TODO: Documentation

--- WP_Mock/DeprecatedListener.php
+++ WP_Mock/DeprecatedListener.php
@@ -9,6 +9,8 @@ class DeprecatedListener {
 	/** @var \PHPUnit_Framework_TestResult */
 	protected $testResult;
 
+	protected $testName;
+
 	public function logDeprecatedCall( $method, array $args = array() ) {
 		$this->calls[] = array( $method, $args );
 	}
@@ -18,7 +20,19 @@ class DeprecatedListener {
 	}
 
 	public function checkCalls() {
-
+		if ( empty( $this->calls ) ) {
+			return;
+		}
+		if ( class_exists( 'PHPUnit_Framework_RiskyTestError' ) ) {
+			throw new \PHPUnit_Framework_RiskyTestError( $this->getMessage() );
+		} else {
+			$this->testResult->addDeprecatedFeature(
+				new \PHPUnit_Util_DeprecatedFeature(
+					$this->getMessage(),
+					debug_backtrace()
+				)
+			);
+		}
 	}
 
 	/**
@@ -26,5 +40,77 @@ class DeprecatedListener {
 	 */
 	public function setTestResult( $testResult ) {
 		$this->testResult = $testResult;
+	}
+
+	/**
+	 * @param mixed $testName
+	 */
+	public function setTestName( $testName ) {
+		$this->testName = $testName;
+	}
+
+	protected function getMessage() {
+		$maxLength = array_reduce( $this->getDeprecatedMethods(), function ( $carry, $item ) {
+				return max( $carry, strlen( $item ) );
+			}, 0 ) + 1;
+		$message   = 'Deprecated WP Mock calls inside ' . $this->testName . ":";
+		foreach ( $this->getDeprecatedMethodsWithArgs() as $method => $args ) {
+			$firstRun = true;
+			$extra    = $maxLength - strlen( $method );
+			foreach ( $args as $arg ) {
+				$message .= "\n  ";
+				if ( $firstRun ) {
+					$message .= $method . str_repeat( ' ', $extra );
+					$firstRun = false;
+					$extra    = $maxLength;
+				} else {
+					$message .= str_repeat( ' ', $extra );
+				}
+				$message .= $arg;
+			}
+		}
+
+		return $message;
+	}
+
+	protected function getDeprecatedMethods() {
+		$methods = array();
+		foreach ( $this->calls as $call ) {
+			$methods = $call[0];
+		}
+
+		return array_unique( $methods );
+	}
+
+	protected function getDeprecatedMethodsWithArgs() {
+		$collection = array();
+		foreach ( $this->calls as $call ) {
+			$method = $call[0];
+			$args   = json_encode( array_map( array( $this, 'scalarizeArg' ), $call[1] ) );
+			if ( empty( $collection[ $method ] ) ) {
+				$collection[ $method ] = array();
+			}
+			$collection[ $method ][] = $args;
+		}
+
+		return array_map( 'array_unique', $collection );
+	}
+
+	protected function scalarizeArg( $value ) {
+		if ( is_scalar( $value ) ) {
+			return $value;
+		} elseif ( is_object( $value ) ) {
+			return '<' . get_class( $value ) . ':' . spl_object_hash( $value ) . '>';
+		} elseif ( is_array( $value ) ) {
+			if ( is_callable( $value ) ) {
+				return '[' . implode( ',', array_map( array( $this, 'scalarizeArg' ), $value ) ) . ']';
+			} else {
+				return 'Array([' . count( $value ) . '] ...)';
+			}
+		} elseif ( is_resource( $value ) ) {
+			return 'Resource';
+		} else {
+			return 'Unknown Value';
+		}
 	}
 }
