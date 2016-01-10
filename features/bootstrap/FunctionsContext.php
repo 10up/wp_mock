2bba418f11ab050ea709664581857d48bb0588fe
John P Bloch <john.bloch@10up.com>
2016-01-09 22:24:12 -0600

    Add a scenario for basic function creation

--- features/bootstrap/FunctionsContext.php
+++ features/bootstrap/FunctionsContext.php
@@ -5,4 +5,28 @@ use Behat\Behat\Context\Context;
 class FunctionsContext implements Context
 {
 
+    /**
+     * @Given function :function does not exist
+     */
+    public function functionDoesNotExist($function)
+    {
+        PHPUnit_Framework_Assert::assertFalse(function_exists($function));
+    }
+
+    /**
+     * @When I mock function :function
+     */
+    public function iMockFunction($function)
+    {
+        WP_Mock::userFunction($function);
+    }
+
+    /**
+     * @Then function :function should exist
+     */
+    public function functionShouldExist($function)
+    {
+        PHPUnit_Framework_Assert::assertTrue(function_exists($function));
+    }
+
 }
--- features/function-mocks.feature
+++ features/function-mocks.feature
@@ -2,3 +2,8 @@ Feature: Function mocking
 	In order to simulate the WordPress functional API
 	As a developer
 	I need to be able to mock WordPress core functions
+
+	Scenario: userFunction creates functions that don't exist
+		Given function wpMockTest does not exist
+		When I mock function wpMockTest
+		Then function wpMockTest should exist
