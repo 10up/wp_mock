77acd80141686a84f45a810352b30214c539ba62
John P Bloch <johnpbloch@gmail.com>
2016-09-20 13:35:56 -0500

    Tweak tests to run as expected

--- tests/ExpectNotAddedTest.php
+++ tests/ExpectNotAddedTest.php
@@ -12,34 +12,42 @@ class ExpectNotAddedTest extends TestCase {
 		$this->callback = array( $this, 'handler' );
 	}
 
-	/* expected failure */
+	/**
+	 * @expectedException \Mockery\Exception\InvalidCountException
+	 */
 	public function test_expect_action_not_added_fails_if_action_was_added() {
 		WP_Mock::expectActionNotAdded(
 			'foo_action', $this->callback
 		);
 
 		add_action( 'foo_action', array( $this, 'handler' ) );
+		WP_Mock::tearDown();
 	}
 
 	public function test_expect_action_not_added_does_not_fail_if_action_was_not_added() {
 		WP_Mock::expectActionNotAdded(
 			'foo_action', $this->callback
 		);
+		WP_Mock::tearDown();
 	}
 
-	/* expected failure */
+	/**
+	 * @expectedException \Mockery\Exception\InvalidCountException
+	 */
 	public function test_expect_filter_not_added_fails_if_filter_was_added() {
 		WP_Mock::expectFilterNotAdded(
 			'foo_filter', $this->callback
 		);
 
 		add_filter( 'foo_filter', array( $this, 'handler' ) );
+		WP_Mock::tearDown();
 	}
 
 	public function test_expect_filter_not_added_does_not_fail_if_filter_was_not_added() {
 		WP_Mock::expectFilterNotAdded(
 			'foo_filter', $this->callback
 		);
+		WP_Mock::tearDown();
 	}
 
 	/* helpers */
