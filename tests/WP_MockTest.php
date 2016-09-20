7f40cdf2da4e4cdf357c4c6eba4f05d4d8c9b0cc
John P Bloch <johnpbloch@gmail.com>
2016-09-20 16:16:15 -0500

    Fix php 5.3 syntax error

--- tests/WP_MockTest.php
+++ tests/WP_MockTest.php
@@ -6,7 +6,7 @@ class WP_MockTest extends PHPUnit_Framework_TestCase
     public function test_userFunction_returns_expectation()
     {
         $this->assertInstanceOf(
-            \Mockery\ExpectationInterface::class,
+            '\Mockery\ExpectationInterface',
             WP_Mock::userFunction('testWpMockFunction')
         );
     }
