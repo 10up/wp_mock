e5072fae0d551cc5868957f3d4726e3c41dc6103
John P Bloch <johnpbloch@gmail.com>
2016-10-07 22:17:30 -0500

    Reformat test

--- tests/WP_MockTest.php
+++ tests/WP_MockTest.php
@@ -1,23 +1,20 @@
 <?php
 
-class WP_MockTest extends PHPUnit_Framework_TestCase
-{
+class WP_MockTest extends PHPUnit_Framework_TestCase {
 
-    /**
-     * @runInSeparateProcess
-     */
-    public function test_strictMode_off_by_default()
-    {
-        WP_Mock::bootstrap();
-        $this->assertFalse(WP_Mock::strictMode());
-    }
+	/**
+	 * @runInSeparateProcess
+	 */
+	public function test_strictMode_off_by_default() {
+		WP_Mock::bootstrap();
+		$this->assertFalse( WP_Mock::strictMode() );
+	}
 
-    public function test_userFunction_returns_expectation()
-    {
-        $this->assertInstanceOf(
-            '\Mockery\ExpectationInterface',
-            WP_Mock::userFunction('testWpMockFunction')
-        );
-    }
+	public function test_userFunction_returns_expectation() {
+		$this->assertInstanceOf(
+			'\Mockery\ExpectationInterface',
+			WP_Mock::userFunction( 'testWpMockFunction' )
+		);
+	}
 
 }
