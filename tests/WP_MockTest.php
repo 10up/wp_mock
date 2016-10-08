5b1916e36440c2f6043c6513837a3b23d2138e2d
John P Bloch <johnpbloch@gmail.com>
2016-10-07 22:08:10 -0500

    Add a method to indicate strict mode

--- tests/WP_MockTest.php
+++ tests/WP_MockTest.php
@@ -3,6 +3,15 @@
 class WP_MockTest extends PHPUnit_Framework_TestCase
 {
 
+    /**
+     * @runInSeparateProcess
+     */
+    public function test_strictMode_off_by_default()
+    {
+        WP_Mock::bootstrap();
+        $this->assertFalse(WP_Mock::strictMode());
+    }
+
     public function test_userFunction_returns_expectation()
     {
         $this->assertInstanceOf(
