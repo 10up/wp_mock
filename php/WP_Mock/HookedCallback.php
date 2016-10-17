1eab20158d98224153d8ab7633ce38ef00987d15
John P Bloch <johnpbloch@gmail.com>
2016-10-16 19:45:02 -0500

    Move strict_check() into HookedCallback

--- php/WP_Mock/HookedCallback.php
+++ php/WP_Mock/HookedCallback.php
@@ -32,6 +32,54 @@ class HookedCallback extends Hook {
 		return parent::safe_offset( $value );
 	}
 
+	/**
+	 * Converts a callable to a string
+	 *
+	 * Closures get returned as 'Closure', objects (those with an __invoke() method get turned into <Class>::__invoke,
+	 * and arrays get turned into <Class>::<method>
+	 *
+	 * @param callable $callback
+	 *
+	 * @return string
+	 */
+	protected function callback_to_string( $callback ) {
+		if ( ! is_string( $callback ) ) {
+			if ( $callback instanceof \Closure ) {
+				$callback = 'Closure';
+			} elseif ( is_object( $callback ) ) {
+				$callback = get_class( $callback ) . '::__invoke';
+			} else {
+				$class  = $callback[0];
+				$method = $callback[1];
+				if ( ! is_string( $class ) ) {
+					$class = get_class( $class );
+				}
+				$callback = "{$class}::$method";
+			}
+		}
+
+		return $callback;
+	}
+
+	/**
+	 * Throw an exception if strict mode is on
+	 *
+	 * @throws \PHPUnit_Framework_ExpectationFailedException
+	 *
+	 * @param callable $callback
+	 */
+	protected function strict_check( $callback ) {
+		if ( \WP_Mock::strictMode() ) {
+			throw new \PHPUnit_Framework_ExpectationFailedException(
+				sprintf(
+					'Unexpected use of add action for action %s with callback %s',
+					$this->name,
+					$this->callback_to_string( $callback )
+				)
+			);
+		}
+	}
+
 }
 
 class HookedCallbackResponder {
