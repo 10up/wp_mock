606e95af81afe1a3e46b492058bc58ec52c0da90
John P Bloch <johnpbloch@gmail.com>
2016-11-13 16:11:40 -0600

    Rewrite common functions test to also make sure the functions aren't defined before bootstrapping

--- tests/FunctionMocksTest.php
+++ tests/FunctionMocksTest.php
@@ -2,27 +2,36 @@
 
 class FunctionMocksTest extends PHPUnit_Framework_TestCase {
 
+	private $common_functions = array(
+		'esc_attr',
+		'esc_html',
+		'esc_js',
+		'esc_textarea',
+		'esc_url',
+		'esc_url_raw',
+		'__',
+		'_e',
+		'_x',
+		'esc_attr__',
+		'esc_attr_e',
+		'esc_attr_x',
+		'esc_html__',
+		'esc_html_e',
+		'esc_html_x',
+		'_n',
+	);
+
 	/**
 	 * @runInSeparateProcess
 	 */
 	public function testCommonFunctionsAreDefined() {
+		// First we assert that all common functions get removed from the returned array. If any one of these functions
+		// doesn't get removed, that means it already exists.
+		$this->assertEmpty( array_filter( $this->common_functions, 'function_exists' ) );
 		WP_Mock::bootstrap();
-		$this->assertTrue( function_exists( 'esc_attr' ) );
-		$this->assertTrue( function_exists( 'esc_html' ) );
-		$this->assertTrue( function_exists( 'esc_js' ) );
-		$this->assertTrue( function_exists( 'esc_textarea' ) );
-		$this->assertTrue( function_exists( 'esc_url' ) );
-		$this->assertTrue( function_exists( 'esc_url_raw' ) );
-		$this->assertTrue( function_exists( '__' ) );
-		$this->assertTrue( function_exists( '_e' ) );
-		$this->assertTrue( function_exists( '_x' ) );
-		$this->assertTrue( function_exists( 'esc_attr__' ) );
-		$this->assertTrue( function_exists( 'esc_attr_e' ) );
-		$this->assertTrue( function_exists( 'esc_attr_x' ) );
-		$this->assertTrue( function_exists( 'esc_html__' ) );
-		$this->assertTrue( function_exists( 'esc_html_e' ) );
-		$this->assertTrue( function_exists( 'esc_html_x' ) );
-		$this->assertTrue( function_exists( '_n' ) );
+		// Now we assert that the array doesn't lose any items after bootstrap, meaning all expected functions got
+		// defined correctly.
+		$this->assertEquals( $this->common_functions, array_filter( $this->common_functions, 'function_exists' ) );
 	}
 
 }
