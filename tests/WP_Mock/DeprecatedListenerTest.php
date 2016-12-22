3a92f1c90bedb0a0ac084354c6eb911642c2b026
John P Bloch <johnpbloch@gmail.com>
2016-12-22 10:21:01 -0600

    Start on tests for deprecated listener class

---
+++ tests/WP_Mock/DeprecatedListenerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace WP_Mock;
+
+use PHPUnit_Framework_TestCase;
+use ReflectionProperty;
+
+class DeprecatedListenerTest extends PHPUnit_Framework_TestCase {
+
+	/** @var DeprecatedListener */
+	protected $object;
+
+	protected function setUp() {
+		$this->object = new DeprecatedListener();
+	}
+
+
+	public function testLogDeprecatedCall() {
+		$method = 'Foobar::asdf' . rand( 0, 9 );
+		$args   = array( rand( 10, 99 ) );
+		$this->object->logDeprecatedCall( $method, $args );
+
+		$this->assertEquals( array( array( $method, $args ) ), $this->getCalls( $this->object ) );
+	}
+
+	protected function getCalls( $listener ) {
+		$prop = new ReflectionProperty( $listener, 'calls' );
+		$prop->setAccessible( true );
+
+		return $prop->getValue( $listener );
+	}
+
+}
