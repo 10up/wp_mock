f44d6ed3c20b7f7b98c1b92ef8839e0943228665
John P Bloch <johnpbloch@gmail.com>
2016-12-22 11:01:37 -0600

    Add a basic test for checkCalls

--- tests/WP_Mock/DeprecatedListenerTest.php
+++ tests/WP_Mock/DeprecatedListenerTest.php
@@ -2,6 +2,9 @@
 
 namespace WP_Mock;
 
+use Mockery;
+use PHPUnit_Framework_Assert;
+use PHPUnit_Framework_RiskyTest;
 use PHPUnit_Framework_TestCase;
 use ReflectionProperty;
 
@@ -14,6 +17,9 @@ class DeprecatedListenerTest extends PHPUnit_Framework_TestCase {
 		$this->object = new DeprecatedListener();
 	}
 
+	public function tearDown() {
+		$this->object->reset();
+	}
 
 	public function testLogDeprecatedCall() {
 		$method = 'Foobar::asdf' . rand( 0, 9 );
@@ -28,6 +34,31 @@ class DeprecatedListenerTest extends PHPUnit_Framework_TestCase {
 		$this->object->reset();
 
 		$this->assertEquals( array(), $this->getCalls( $this->object ) );
+	}
+
+	public function testCheckCalls_scalar_only() {
+		$this->object->logDeprecatedCall( 'FooBar::bazBat', array( 'string', true, 42 ) );
+		$this->object->setTestName( 'TestName' );
+		$testCase = Mockery::mock( 'PHPUnit_Framework_TestCase' );
+		/** @var PHPUnit_Framework_TestCase $testCase */
+		$this->object->setTestCase( $testCase );
+		$result = Mockery::mock( 'PHPUnit_Framework_TestResult' );
+		$result->shouldReceive( 'addFailure' )
+			->once()
+			->andReturnUsing( function ( $case, $exception, $int ) use ( $testCase ) {
+				PHPUnit_Framework_Assert::assertSame( $testCase, $case );
+				PHPUnit_Framework_Assert::assertTrue( $exception instanceof PHPUnit_Framework_RiskyTest );
+				$message = <<<EOT
+Deprecated WP Mock calls inside TestName:
+  FooBar::bazBat ["string",true,42]
+EOT;
+				PHPUnit_Framework_Assert::assertEquals( $message, $exception->getMessage() );
+				PHPUnit_Framework_Assert::assertTrue( 0 === $int );
+			} );
+		/** @var \PHPUnit_Framework_TestResult $result */
+		$this->object->setTestResult( $result );
+
+		$this->object->checkCalls();
 	}
 
 	protected function getCalls( $listener ) {
