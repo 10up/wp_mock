0cf27f2f7b9dc68ebbf8f811cf47f4f0ede18b72
Fulvio Notarstefano <unfulvio@godaddy.com>
2023-06-29 01:12:25 +0800

    Update HandlerTest

--- tests/Unit/WP_Mock/Functions/HandlerTest.php
+++ tests/Unit/WP_Mock/Functions/HandlerTest.php
@@ -4,6 +4,7 @@ namespace Unit\WP_Mock\Functions;
 
 use Exception;
 use ReflectionProperty;
+use WP_Mock;
 use WP_Mock\Functions\Handler;
 use WP_Mock\Tests\WP_MockTestCase;
 
@@ -15,6 +16,9 @@ final class HandlerTest extends WP_MockTestCase
     /**
      * @covers \WP_Mock\Functions\Handler::registerHandler()
      *
+     * @preserveGlobalState disabled
+     * @runInSeparateProcess
+     *
      * @return void
      * @throws Exception
      */
@@ -24,9 +28,9 @@ final class HandlerTest extends WP_MockTestCase
 
         $this->assertSame([], $property->getValue());
 
-        $functionName = 'foo';
+        $functionName = 'test_function';
         $callback = function () {
-            return 'bar';
+            return null;
         };
 
         Handler::registerHandler($functionName, $callback);
@@ -34,30 +38,110 @@ final class HandlerTest extends WP_MockTestCase
         $this->assertSame([$functionName => $callback], $property->getValue());
     }
 
+    /**
+     * @covers \WP_Mock\Functions\Handler::handleFunction()
+     *
+     * @return void
+     * @throws Exception
+     */
     public function testCanHandleFunction(): void
     {
+        $this->assertNull(Handler::handleFunction('invalid'));
 
+        $functionName = 'test_function';
+        $callback = function ($arg) {
+            return $arg;
+        };
+
+        Handler::registerHandler($functionName, $callback);
+
+        $this->assertEquals('test-arg', Handler::handleFunction($functionName, ['test-arg']));
     }
 
+    /**
+     * @covers \WP_Mock\Functions\Handler::handlerExists()
+     *
+     * @preserveGlobalState disabled
+     * @runInSeparateProcess
+     *
+     * @return void
+     * @throws Exception
+     */
     public function testCanDetermineHandlerExists(): void
     {
+        $functionName = 'test_function';
+        $callback = function () {
+            return null;
+        };
 
+        $this->assertFalse(Handler::handlerExists($functionName));
+
+        Handler::registerHandler($functionName, $callback);
+
+        $this->assertTrue(Handler::handlerExists($functionName));
     }
 
+    /**
+     * @covers \WP_Mock\Functions\Handler::cleanup()
+     *
+     * @preserveGlobalState disabled
+     * @runInSeparateProcess
+     *
+     * @return void
+     * @throws Exception
+     */
     public function testCanCleanup(): void
     {
+        $property = new ReflectionProperty(Handler::class, 'handlers');
 
+        $functionName = 'test_function';
+        $callback = function () {
+            return null;
+        };
+
+        Handler::registerHandler($functionName, $callback);
+        Handler::cleanup();
+
+        $this->assertSame([], $property->getValue());
     }
 
+    /**
+     * @covers \WP_Mock\Functions\Handler::handlePredefinedReturnFunction()
+     *
+     * @preserveGlobalState disabled
+     * @runInSeparateProcess
+     *
+     * @return void
+     * @throws Exception
+     */
     public function testCanHandlePredefinedReturnFunction(): void
     {
+        WP_Mock::bootstrap();
+        WP_Mock::userFunction('test_function')
+            ->once()
+            ->andReturn('test-arg');
 
+        $this->assertSame('test-arg', test_function());
     }
 
+    /**
+     * @covers \WP_Mock\Functions\Handler::handlePredefinedEchoFunction()
+     *
+     * @preserveGlobalState disabled
+     * @runInSeparateProcess
+     *
+     * @return void
+     * @throws Exception
+     */
     public function testCanHandlePredefinedEchoFunction(): void
     {
+        WP_Mock::bootstrap();
+        WP_Mock::echoFunction('test_function');
 
+        ob_start();
+
+        test_function('test');
+
+        $this->assertSame('test', ob_get_clean());
     }
-
-
 }
