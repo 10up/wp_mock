3cd3332ba60c63b155b530939488d00c7d6fe4cf
Fulvio Notarstefano <unfulvio@godaddy.com>
2023-07-06 11:10:59 +0800

    Add setInaccessiblePropertyValue helper method

--- tests/Unit/WP_Mock/Traits/AccessInaccessibleClassMembersTraitTest.php
+++ tests/Unit/WP_Mock/Traits/AccessInaccessibleClassMembersTraitTest.php
@@ -42,6 +42,19 @@ final class AccessInaccessibleClassMembersTraitTest extends TestCase
     }
 
     /**
+     * @covers \WP_Mock\Traits\AccessInaccessibleClassMembersTrait::setInaccessiblePropertyValue()
+     *
+     * @return void
+     */
+    public function testCanSetInaccessiblePropertyValue() : void
+    {
+        $instance = $this->getInstance('foo');
+        $property = $instance->setInaccessiblePropertyValue($instance, 'property', 'bar');
+
+        $this->assertEquals('bar', $property->getValue($instance));
+    }
+
+    /**
      * @covers \WP_Mock\Traits\AccessInaccessibleClassMembersTrait::getInaccessiblePropertyValue()
      *
      * @return void
