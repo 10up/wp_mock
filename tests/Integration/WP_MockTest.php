93465a80c6b2346f868ef2784fabf31f6b51646d
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2024-06-25 01:35:47 +0900

    Fix failing analysis

--- tests/Integration/WP_MockTest.php
+++ tests/Integration/WP_MockTest.php
@@ -130,7 +130,6 @@ class WP_MockTest extends WP_MockTestCase
      * @preserveGlobalState disabled
      *
      * @return void
-     * @throws Exception
      */
     public function testDefaultFailsInStrictMode(): void
     {
